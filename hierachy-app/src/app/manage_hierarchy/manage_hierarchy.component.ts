a9b03d40e1edd03823a3b0fdc4c4e64a031b3077
Bhakti-predigle <bkhedkar79@gmail.com>
2025-05-15 16:58:06 +0530

    layout of whole hierarchy page

--- hierachy-app/src/app/manage_hierarchy/manage_hierarchy.component.ts
+++ hierachy-app/src/app/manage_hierarchy/manage_hierarchy.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ButtonComponent } from '@predigle/micro';
+import { TopNavBarComponent } from '../top-nav-bar/top-nav-bar.component';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @Component({
@@ -8,7 +10,7 @@ import { ButtonComponent } from '@predigle/micro';
   selector: 'app-manage_hierarchy',
   templateUrl: './manage_hierarchy.component.html',
   styleUrls: ['./manage_hierarchy.component.scss'],
-  imports : [CommonModule,ButtonComponent],
+  imports : [CommonModule,ButtonComponent,TopNavBarComponent,MatIconModule],
 })
 export class Manage_hierarchyComponent implements OnInit {
   selectedTab: string = 'manageNode';
